65d4e489ec658aa6f7d7e009050a8b5d65f13ff0
Astha Bhardwaj <asthabhardwaj529@gmail.com>
2024-10-14 23:43:21 +0530

    data type

---
+++ data_type_conversion.js
@@ -0,0 +1,40 @@
+// // datatype conversion
+
+// let score="33abc"
+// let score="33abc"
+// let score=null
+// let score=undefined
+// let score=true
+// let valueInScore= Number(score)
+
+
+// console.log(valueInScore);
+// console.log(typeof score);
+// console.log(typeof valueInScore);
+
+// // datatype conversion
+// // 33  ==> number
+// // 33abc ==>NaN
+// // undefined ==> NaN
+// // null ==>0
+// // true, false=1,0
+
+// let isLoggedIn = 1
+// let booleanIsloggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+// console.log(typeof isLoggedIn);
+
+// //datatype conversion
+
+// // 1 => true  ,  0 => false
+// // "" =>false , "deepak" => true
+
+let someNumber= 987654
+let stringIsNumber = String(someNumber)
+console.log(stringIsNumber);
+console.log(typeof stringIsNumber);
+console.log(typeof someNumber);
+
+
+
+
